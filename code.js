101b60becf2b671fde4c51720559d724f680038c
Tom <tomlitherland@outlook.com>
2025-12-15 20:45:42 +0000

    Add basic vertical page example

--- code.js
+++ code.js
@@ -1,3 +1,33 @@
+var currentPage = 0;
+
 function onLoad() {
-    document.getElementById("intro").innerText = "ASOS Wrapped 2025";
+    let pages = Array.from(document.getElementsByClassName("page"));
+
+    pages.forEach(element => {
+        element.classList.add("unopenedPage");
+    });
+    pages[0].classList.remove("unopenedPage");
+    pages[0].classList.add("openPage");
+}
+
+function nextPage() {
+    let pages = Array.from(document.getElementsByClassName("page"));
+
+    if (currentPage + 1 === pages.length) {
+        // end
+        return;
+    }
+
+    pages[currentPage].classList.remove("openPage");
+    // pages[currentPage].classList.remove("unopenedPage");
+    pages[currentPage].classList.add("closedPage");
+
+    currentPage++;
+    if (currentPage == pages.length) {
+        return;
+    }
+    // pages[currentPage].classList.remove("unopenedPage");
+    pages[currentPage].classList.remove("closedPage");
+    pages[currentPage].classList.add("openPage");
+
 }
